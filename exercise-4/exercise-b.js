5f1e37ee5cf68697aa683ab740da6776a888a94e
Ever <redsoul10@hotmail.com>
2021-03-25 02:29:05 -0300

    Exercise 4

---
+++ exercise-4/exercise-b.js
@@ -0,0 +1,22 @@
+/*
+Crear una variable 
+“Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+*/
+
+var Age = 2
+
+if( Age < 2){
+alert("Bebe")
+}else if( Age < 12){
+alert("Nino")
+}else if( Age < 19){
+alert("Adolescente")
+}else if( Age < 30){
+alert("Joven")
+}else if(Age < 60){
+alert("Adulto")
+}else if( Age < 75){
+alert("Adulto mayor")
+}else{
+alert("Anciano")
+}
